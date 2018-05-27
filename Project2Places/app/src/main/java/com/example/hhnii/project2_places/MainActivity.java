2702de516e2883faafabd0ef4eb9601da3ca64fb
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2018-05-27 01:43:17 -0500

    Adds handlers for about activity and exit action from main activity

--- Project2Places/app/src/main/java/com/example/hhnii/project2_places/MainActivity.java
+++ Project2Places/app/src/main/java/com/example/hhnii/project2_places/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hhnii.project2_places;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aboutButton, exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        aboutButton= findViewById(R.id.aboutBtn);
+        aboutButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        showAbout(null);
+                    }
+                }
+        );
+        exitButton= findViewById(R.id.exitBtn);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -48,4 +60,10 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void showAbout(View view){
+        Intent intent= new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
 }
