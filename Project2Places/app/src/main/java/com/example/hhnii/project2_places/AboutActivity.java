09a14edd868229c1cc0ee630ee5b351e794f531b
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2018-05-27 04:36:44 -0500

    Adds layout and resources for view place activity (TO_DO: add translation for fields)

--- Project2Places/app/src/main/java/com/example/hhnii/project2_places/AboutActivity.java
+++ Project2Places/app/src/main/java/com/example/hhnii/project2_places/AboutActivity.java
@@ -2,6 +2,7 @@ package com.example.hhnii.project2_places;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AboutActivity extends Activity {
 
--- Project2Places/app/src/main/java/com/example/hhnii/project2_places/MainActivity.java
+++ Project2Places/app/src/main/java/com/example/hhnii/project2_places/MainActivity.java
@@ -57,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if(id== R.id.about_settings){
+            showAbout(null);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
