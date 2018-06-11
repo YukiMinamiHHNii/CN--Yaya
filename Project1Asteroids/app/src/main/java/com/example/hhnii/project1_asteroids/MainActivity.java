1e9638446283a4efc38e58914bd96ef25b7678ab
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2018-06-10 23:20:28 -0500

    Adds menu with overflow to main layout

--- Project1Asteroids/app/src/main/java/com/example/hhnii/project1_asteroids/MainActivity.java
+++ Project1Asteroids/app/src/main/java/com/example/hhnii/project1_asteroids/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.hhnii.project1_asteroids;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar= findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case(R.id.about_action):
+                Log.d("aboutAction", "microwave");
+                break;
+
+            case(R.id.exit_action):
+                finish();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
